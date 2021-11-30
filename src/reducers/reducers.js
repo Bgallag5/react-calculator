d9df30b3466ffffac8b400b335c2051cf93d506c
bengallagher <bgallag5@gmail.com>
2021-11-30 12:46:25 -0500

    clean up

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,6 +1,3 @@
-// reducer functions to handle each defined action
-// one reducer func with switch cases to return updated state
-// import actions first
 import {
   ADD_OPERATION,
   CALCULATE,
@@ -13,21 +10,22 @@ import {
 export const reducer = (state, { type, payload }) => {
   switch (type) {
     case ADD_DIGIT:
-        if (payload.digit === '.' && state.bottomDisplay != null){
-            if([...state.bottomDisplay].includes('.')) return state;
-            return {
-                ...state,
-                bottomDisplay: `${state.bottomDisplay || ""}${payload.digit}`,
-            }
-        }; 
+      //prevent adding more than one '.'
+      if (payload.digit === "." && state.bottomDisplay != null) {
+        if ([...state.bottomDisplay].includes(".")) return state;
+        return {
+          ...state,
+          bottomDisplay: `${state.bottomDisplay || ""}${payload.digit}`,
+        };
+      }
       return {
         ...state,
         bottomDisplay: `${state.bottomDisplay || ""}${payload.digit}`,
       };
     case ADD_OPERATION:
-      // == null retruns true for both null AND undefined values
-      if (state.topDisplay == null && state.bottomDisplay == null){
-          return state;
+      // == null returns true for both null AND undefined values
+      if (state.topDisplay == null && state.bottomDisplay == null) {
+        return state;
       }
       if (state.bottomDisplay == null) {
         return {
@@ -43,53 +41,46 @@ export const reducer = (state, { type, payload }) => {
           symbol: payload.symbol,
         };
       }
+      //if value in topDisplay: run calculation
       return {
         ...state,
         topDisplay: calculate(state),
         symbol: payload.symbol,
-        bottomDisplay: '',
+        bottomDisplay: "",
       };
-      case CLEAR: 
+    case CLEAR:
       return {
-          ...state,
-          symbol: null,
-          topDisplay: null,
-          bottomDisplay: null
+        ...state,
+        symbol: null,
+        topDisplay: null,
+        bottomDisplay: null,
       };
-      case CALCULATE: 
-      // conditionals: if I press equal button when...
-      // no top and bottom display: return state
-      // no bottom, return state
-      // no top, return state
-      // only run on having all state? 
+    case CALCULATE:
       if (!state.topDisplay || !state.bottomDisplay) return state;
 
       return {
-          ...state,
-          bottomDisplay: calculate(state),
-          topDisplay: null,
-          symbol: null  
+        ...state,
+        bottomDisplay: calculate(state),
+        topDisplay: null,
+        symbol: null,
       };
-      case DELETE_DIGIT: 
-      if (state.bottomDisplay == null ) return state; 
-      // spread and loop over current bottomVal and remove the last digit, then return as bottomVal
-      const newVal = [...state.bottomDisplay].slice(0, -1).join('');
+    case DELETE_DIGIT:
+      if (state.bottomDisplay == null) return state;
+      // spread and loop over current bottomVal and remove the last digit, then return as new bottomVal
+      const newVal = [...state.bottomDisplay].slice(0, -1).join("");
       return {
-          ...state,
-          bottomDisplay: newVal
-      } 
+        ...state,
+        bottomDisplay: newVal,
+      };
     default:
       return state;
   }
 };
 
 function calculate(state) {
-  //use state.symbol as operator and top/bottom as values, then in return in reducer set symbol to payload.symbol
-
   let topValue = parseFloat(state.topDisplay);
   let bottomValue = parseFloat(state.bottomDisplay);
-  console.log(topValue, bottomValue);
-  //return calculated value as a string 
+  //return calculated value as a string
   switch (state.symbol) {
     case "+":
       return (topValue + bottomValue).toString();
@@ -103,8 +94,3 @@ function calculate(state) {
       return state;
   }
 }
-
-//also function to handle calculations
-
-
-//RULE: a == null returns true if the value of a is null OR undefined!!!
